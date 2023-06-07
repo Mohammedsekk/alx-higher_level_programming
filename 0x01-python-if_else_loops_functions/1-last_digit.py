5c924ac1bc92cc0b567208611c10b77536afabfe
Mohammedsekk <seksimo2001@gmail.com>
2023-06-07 00:56:09 +0000

    fixed Pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-description = ""
-if number > 5:
-    description = "and is greater than 5"
-elif number == 0:
-    description = "and is 0"
-elif number > 6 and number != 0:
-    description = "and is less than 6 and not 0"
-print(f"last digit of {number} is {last_digit} {description}")
+n = number
+check_sig = 0
+if (n < 0):
+    n *= -1
+    check_sig = 1
+lastd = n % 10
+if (check_sig):
+    lastd *= -1
+    n *= -1
+if (lastd > 5):
+    print("Last digit of %d is %d and is greater than 5" % (n, lastd))
+elif (lastd == 0):
+    print("Last digit of %d is %d and is 0" % (n, lastd))
+elif (lastd < 6 and lastd != 0):
+    print(f"Last digit of {n:d} is {lastd:d} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author-Mohammed Sekkat
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(97, 123):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    print("{}".format(chr(i)), end="")
